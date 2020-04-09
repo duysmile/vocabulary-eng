d011748712dd994474bcbc216b4a488f79fa0680
Duy Nguyen <duy.nguyen@gonjoy.com>
2020-04-09 16:32:15 +0700

    update extension

--- extension/content.js
+++ extension/content.js
@@ -16,16 +16,16 @@ function receiveMessage(request, sender, sendResponse) {
     const body = document.querySelector('body');
     const div = document.createElement('div');
     div.innerHTML = `
-    <div style="width: 300px; height: 90px; border-radius: 20px; display: flex; background-image: linear-gradient(120deg, #84fab0 0%, #8fd3f4 100%); opacity: 0.7; box-shadow: 4px 4px rgba(0, 98, 90, 0.4), 8px 8px rgba(0, 98, 90, 0.3); font-family: sans-serif; justify-content: center; align-items: center; flex-direction: column;  color: #272727;">
-        <div style="font-weight: bold; display: flex; align-items: center">
-            <span style=" background-color: white; padding: 6px; border-radius: 8px; justify-content: center; display: flex">
+    <div style="font-size: 16px; min-width: 300px; height: 90px; border-radius: 20px; display: flex; background-image: linear-gradient(120deg, #84fab0 0%, #8fd3f4 100%); opacity: 0.8; box-shadow: 4px 4px rgba(0, 98, 90, 0.4), 8px 8px rgba(0, 98, 90, 0.3); font-family: sans-serif; justify-content: center; align-items: center; flex-direction: column;  color: #272727;">
+        <div style="font-weight: bold; display: flex; align-items: center; margin: 5px;">
+            <span style="background-color: white; padding: 6px; border-radius: 8px; justify-content: center; display: flex">
                 ${words[0]} ${words[1]}
             </span>
             <span style="display: flex; align-items: center; padding: 6px; word-break: break-all;">
                 ${words[2]}
             </span>
         </div>
-        <div style="font-size: 0.8rem; margin-top: 5px; word-break: break-all;">
+        <div style="font-size: 12px; margin: 5px; word-break: break-all;">
             ${words[4]}
         </div>
     </div>
@@ -34,11 +34,11 @@ function receiveMessage(request, sender, sendResponse) {
     div.style.position = 'fixed';
     div.style.bottom = '20px';
     div.style.right = '20px';
-    div.style.width = '300px';
     div.style.height = '90px';
     div.style.zIndex = '10000';
+    div.style.minWidth = '300px';
     div.onclick = (e) => {
-        e.target.remove();
+        document.getElementById('duynguyen-popup').remove();
     }
     body.appendChild(div);
 }
