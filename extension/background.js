26275a4fc07a942c6e2c45534d6792f93cc223a3
duy21 <duy210697@gmail.com>
2020-04-09 17:52:07 +0700

    add extension

--- extension/background.js
+++ extension/background.js
@@ -1,10 +1,43 @@
-console.log('it works');
+const randomNumber = parseInt(Math.random() * 46);
+const baseUrl = `https://api.github.com/repos/duysmile/vocabulary-eng/contents/vocabulary/v-${randomNumber}.json`
+const DURATION_POPUP = 1000 * 10;
 
-chrome.browserAction.onClicked.addListener(btnClicked);
+fetch(baseUrl).then(res => {
+    return res.json();
+}).then(data => {
+    const { content } = data;
+    const decodedContent = b64ToUtf8(content);
+    const dataContent = JSON.parse(decodedContent);
+    const range = dataContent.length;
 
-// mua mouse may tinh cho Hang
+    chrome.browserAction.onClicked.addListener(btnClicked);
 
-function btnClicked(tab) {
-    console.log('yeah it works well!');
-    chrome.tabs.sendMessage(tab.id, { msg: 'I love you so much <3' });
-}
+    function btnClicked(tab) {
+        chrome.tabs.sendMessage(tab.id, {
+            msg: 'I love you so much <3',
+            action: 'toggle',
+        });
+    }
+
+    setInterval(dailyVoca, DURATION_POPUP);
+    
+    function dailyVoca() {
+        const randomWord = parseInt(Math.random() * range);
+        const word = dataContent[randomWord];
+        const params = {
+            active: true,
+            currentWindow: true,
+        }
+        chrome.tabs.query(params, (tabs) => {
+            const tabId = tabs[0].id;
+            chrome.tabs.sendMessage(tabId, { word });
+        });
+    }
+
+    function b64ToUtf8(str) {
+        return decodeURIComponent(escape(window.atob(str)));
+    }
+
+    dailyVoca();
+});
+
--- extension/content.js
+++ extension/content.js
@@ -1,5 +1,43 @@
 chrome.runtime.onMessage.addListener(receiveMessage);
 
 function receiveMessage(request, sender, sendResponse) {
-    console.log(request);
+    console.log('received');
+    const { word } = request;
+    if (!word) {
+        return;
+    }
+    const words = word.split('|')
+
+    const currentPopup = document.getElementById('duynguyen-popup');
+    if (currentPopup) {
+        currentPopup.remove();
+    }
+
+    const body = document.querySelector('body');
+    const div = document.createElement('div');
+    div.innerHTML = `
+    <div style="width: 300px; height: 90px; border-radius: 20px; display: flex; background-image: linear-gradient(120deg, #84fab0 0%, #8fd3f4 100%); opacity: 0.7; box-shadow: 4px 4px rgba(0, 98, 90, 0.4), 8px 8px rgba(0, 98, 90, 0.3); font-family: sans-serif; justify-content: center; align-items: center; flex-direction: column;  color: #272727;">
+        <div style="font-weight: bold; display: flex; align-items: center">
+            <span style=" background-color: white; padding: 6px; border-radius: 8px; justify-content: center; display: flex">
+                ${words[0]} ${words[1]}
+            </span>
+            <span style="display: flex; align-items: center; padding: 6px;">
+                ${words[2]}
+            </span>
+        </div>
+        <div style="font-size: 0.8rem; margin-top: 5px">
+            ${words[4]}
+        </div>
+    </div>
+    `;
+    div.id = 'duynguyen-popup';
+    div.style.position = 'fixed';
+    div.style.bottom = '20px';
+    div.style.right = '20px';
+    div.style.width = '300px';
+    div.style.height = '90px';
+    div.onclick = (e) => {
+        e.target.remove();
+    }
+    body.appendChild(div);
 }
